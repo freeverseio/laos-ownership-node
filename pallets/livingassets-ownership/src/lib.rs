5ad96346773d89e474141156cf45576498c87679
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-07-18 10:30:02 +0100

    + doc

--- pallets/livingassets-ownership/src/lib.rs
+++ pallets/livingassets-ownership/src/lib.rs
@@ -29,7 +29,7 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-        /// todo
+        /// Collection id type 
         type CollectionId: Member + Parameter + MaxEncodedLen + Copy;
     }
 
