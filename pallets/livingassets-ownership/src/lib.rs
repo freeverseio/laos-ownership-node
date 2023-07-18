2c9a482af909836d4152c758c26f1798af26a725
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-07-18 08:08:04 +0100

    better doc

--- pallets/livingassets-ownership/src/lib.rs
+++ pallets/livingassets-ownership/src/lib.rs
@@ -69,7 +69,53 @@ pub mod pallet {
         }
     }
 
-    /// LivingAssetsOwnership trait:w
+/// The `LivingAssetsOwnership` trait provides an interface for managing collections in a
+/// decentralized and non-fungible asset management system. This system allows for the creation of
+/// collections, each of which can be owned by a unique `AccountId`.
+///
+/// A collection in this context can be thought of as a container for non-fungible assets. 
+/// Each collection has an associated `collection_id` which is a unique identifier for the collection 
+/// and can be used to retrieve the owner of the collection.
+///
+/// # Methods
+/// 
+/// - `owner_of_collection(collection_id: u64) -> Option<AccountId>`: This method retrieves the owner
+/// of a collection given its `collection_id`. If no collection exists with the provided `collection_id`,
+/// the method returns `None`.
+///
+/// - `create_collection(collection_id: u64, who: AccountId) -> DispatchResult`: This method creates a
+/// new collection with the specified `collection_id` and assigns ownership to the provided `AccountId`.
+/// If a collection already exists with the provided `collection_id`, the method will return an error.
+///
+/// # Examples
+///
+/// ```
+/// use your_pallet::LivingAssetsOwnership;
+///
+/// // assuming `Pallet` implements `LivingAssetsOwnership`
+/// let collection_id = 1;
+/// let account_id = ...; // assume we have a valid account_id
+/// 
+/// // create a new collection and assign ownership
+/// Pallet::create_collection(collection_id, account_id.clone()).expect("Failed to create collection");
+///
+/// // retrieve the owner of the collection
+/// assert_eq!(Pallet::owner_of_collection(collection_id), Some(account_id));
+/// ```
+///
+/// # Errors
+///
+/// - `CollectionAlreadyExists`: This error is returned by the `create_collection` method when a collection
+/// with the provided `collection_id` already exists.
+///
+pub trait LivingAssetsOwnership<AccountId> {
+    /// Get owner of collection
+    fn owner_of_collection(collection_id: u64) -> Option<AccountId>;
+
+    /// Create collection
+    fn create_collection(collection_id: u64, who: AccountId) -> DispatchResult;
+}
+
     pub trait LivingAssetsOwnership<AccountId> {
         /// Get owner of collection
         fn owner_of_collection(collection_id: u64) -> Option<AccountId>;
