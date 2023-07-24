49cefaa653075e0e5f87edf8c1267518d0eca931
Dastan Samatov <dastan@freeverse.io>
2023-07-24 18:16:47 +0600

    Fix fmt

--- node/src/command.rs
+++ node/src/command.rs
@@ -1,11 +1,11 @@
 use std::net::SocketAddr;
 
-use parity_scale_codec::Encode;
 use cumulus_client_cli::generate_genesis_block;
 use cumulus_primitives_core::ParaId;
 use frame_benchmarking_cli::{BenchmarkCmd, SUBSTRATE_REFERENCE_HARDWARE};
 use log::{info, warn};
 use parachain_template_runtime::Block;
+use parity_scale_codec::Encode;
 use polkadot_service::RococoChainSpec;
 use sc_cli::{
 	ChainSpec, CliConfiguration, DefaultConfigurationValues, ImportParams, KeystoreParams,
