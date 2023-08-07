9dc0c14d28bf329dad6015f0889151a7cf6590a1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-08-07 12:08:26 +0200

    is_erc721_contract is not member of PrecompoileSet

--- runtime/src/precompiles/mod.rs
+++ runtime/src/precompiles/mod.rs
@@ -40,14 +40,6 @@ type Erc721 = Erc721Precompile<
 	pallet_living_assets_ownership::Pallet<crate::Runtime>,
 >;
 
-impl<Runtime> FrontierPrecompiles<Runtime> {
-	fn is_erc721_address(&self, address: H160) -> bool {
-		let first_byte = address.0[0];
-		// Check if the first bit is set to 1
-		first_byte & 0x80 == 0x80
-	}
-}
-
 impl<Runtime> PrecompileSet for FrontierPrecompiles<Runtime>
 where
 	Runtime: pallet_evm::Config + pallet_living_assets_ownership::Config,
@@ -64,13 +56,13 @@ where
 			// a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
 			a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
 			a if a == hash(1026) => Some(LivingAssetsPrecompile::execute(handle)),
-			a if self.is_erc721_address(a) => Some(Erc721::execute(handle)),
+			a if is_erc721_address(a) => Some(Erc721::execute(handle)),
 			_ => None,
 		}
 	}
 
 	fn is_precompile(&self, address: H160, _gas: u64) -> IsPrecompileResult {
-		if self.is_erc721_address(address) {
+		if is_erc721_address(address) {
 			return IsPrecompileResult::Answer { is_precompile: true, extra_cost: 0 };
 		}
 
@@ -83,6 +75,12 @@ where
 
 fn hash(a: u64) -> H160 {
 	H160::from_low_u64_be(a)
+}
+
+fn is_erc721_address(address: H160) -> bool {
+	let first_byte = address.0[0];
+	// Check if the first bit is set to 1
+	first_byte & 0x80 == 0x80
 }
 
 #[cfg(test)]
