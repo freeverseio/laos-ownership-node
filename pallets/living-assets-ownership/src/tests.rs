e76a68065f3b85a74628f2fdca042f323eb319ed
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-08-16 14:20:29 +0200

    test moved where concreate impl is tested

--- pallets/living-assets-ownership/src/tests.rs
+++ pallets/living-assets-ownership/src/tests.rs
@@ -118,3 +118,127 @@ fn test_is_collection_address_invalid() {
 
 	assert!(!is_collection_address(invalid_address));
 }
+
+mod traits {
+	use super::*;
+	use crate::{
+		traits::{CollectionManager, Erc721},
+		Erc721Error, Event,
+	};
+	use frame_support::{assert_err, assert_ok};
+
+	#[test]
+	fn base_uri_of_unexistent_collection_is_none() {
+		new_test_ext().execute_with(|| {
+			assert_eq!(<LivingAssetsModule as CollectionManager<AccountId>>::base_uri(0), None);
+			assert_eq!(<LivingAssetsModule as CollectionManager<AccountId>>::base_uri(1), None);
+		});
+	}
+
+	#[test]
+	fn create_new_collection_should_emit_an_event() {
+		new_test_ext().execute_with(|| {
+			// Go past genesis block so events get deposited
+			System::set_block_number(1);
+
+			assert_ok!(<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+				ALICE,
+				BaseURI::default(),
+			));
+			System::assert_last_event(
+				Event::CollectionCreated { collection_id: 0, who: ALICE }.into(),
+			);
+		});
+	}
+
+	#[test]
+	fn living_assets_ownership_trait_id_of_new_collection_should_be_consecutive() {
+		new_test_ext().execute_with(|| {
+			assert_eq!(
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default()
+				)
+				.unwrap(),
+				0
+			);
+			assert_eq!(
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default()
+				)
+				.unwrap(),
+				1
+			);
+			assert_eq!(
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default()
+				)
+				.unwrap(),
+				2
+			);
+			assert_eq!(
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default()
+				)
+				.unwrap(),
+				3
+			);
+			assert_eq!(
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default()
+				)
+				.unwrap(),
+				4
+			);
+			assert_eq!(
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default()
+				)
+				.unwrap(),
+				5
+			);
+		});
+	}
+
+	#[test]
+	fn living_assets_ownership_trait_should_set_base_uri_when_creating_new_collection() {
+		let base_uri = BaseURI::try_from("https://example.com/".as_bytes().to_vec()).unwrap();
+
+		new_test_ext().execute_with(|| {
+			assert_ok!(<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+				ALICE,
+				base_uri.clone()
+			));
+			assert_eq!(LivingAssetsModule::collection_base_uri(0).unwrap(), base_uri);
+		});
+	}
+
+	#[test]
+	fn owner_of_asset_of_unexistent_collection_should_error() {
+		new_test_ext().execute_with(|| {
+			let result = <LivingAssetsModule as Erc721>::owner_of(0, 2.into());
+			assert_err!(result, Erc721Error::UnexistentCollection);
+		});
+	}
+
+	#[test]
+	fn erc721_owner_of_asset_of_collection() {
+		new_test_ext().execute_with(|| {
+			let collection_id =
+				<LivingAssetsModule as CollectionManager<AccountId>>::create_collection(
+					ALICE,
+					BaseURI::default(),
+				)
+				.unwrap();
+			assert_eq!(
+				<LivingAssetsModule as Erc721>::owner_of(collection_id, 2.into()).unwrap(),
+				H160::from_low_u64_be(0x0000000000000002)
+			);
+		});
+	}
+}
