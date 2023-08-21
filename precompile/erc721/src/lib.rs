babd2f4342799e24132f8aeee65912b77ddddeeb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-08-21 16:00:22 +0200

    fix compilation

--- precompile/erc721/src/lib.rs
+++ precompile/erc721/src/lib.rs
@@ -26,10 +26,8 @@ where
 {
 	fn execute(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
 		// collection id is encoded into the contract address
-		let collection_id = address_to_collection_id(handle.code_address()).map_err(|_| Err(revert("invalid collection address")))?;
-			Ok(collection_id) => collection_id,
-			Err(_) => return Err(revert("invalid collection address")),
-		};
+		let collection_id = address_to_collection_id(handle.code_address())
+			.map_err(|_| revert("invalid collection address"))?;
 
 		let selector = handle.read_selector()?;
 
